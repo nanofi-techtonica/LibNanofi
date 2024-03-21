cbe32555e91fcb779a1d773d879cbc38f273b3d9
nanofi <nanogenomu@gmail.com>
2024-03-22 04:42:00 +0900

    build: Add a publish proces for bumping version

---
+++ LibNanofi.Build/Tasks/MakeBumpedVersionCommit.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LibNanofi.Build.Tasks
+{
+    public class MakeBumpedVersionCommit : Task
+    {
+        [Required]
+        public string RootDir { get; set; }
+        public string ChangelogFile { get; set; } = "CHANGELOG.md";
+        [Output]
+        public string NextVersion { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Making bumped version commit...");
+            try
+            {
+                var nextVersion = Util.ExecuteCommand("git", "cliff --bumped-version", RootDir).Trim();
+                Util.ExecuteCommand("git", $"cliff --bump -o {ChangelogFile}", RootDir);
+                Util.ExecuteCommand("git", $"add {ChangelogFile}", RootDir);
+                Util.ExecuteCommand("git", $"commit -m \"chore(release): update {ChangelogFile} for {nextVersion}\"", RootDir);
+                NextVersion = nextVersion;
+            }
+            catch (Exception e)
+            {
+                Log.LogErrorFromException(e);
+            }
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
---
+++ LibNanofi.Build/Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LibNanofi.Build
+{
+    internal static class Util
+    {
+        public static string ExecuteCommand(string command, string args, string workingDir)
+        {
+            using (var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = workingDir,
+                }
+            })
+            {
+                process.Start();
+                process.WaitForExit();
+                string result = process.StandardOutput.ReadToEnd();
+                if (process.ExitCode != 0)
+                {
+                    var message = process.StandardError.ReadToEnd();
+                    throw new Exception($"Command '{command} {args}' failed with exit code {process.ExitCode}:\n{message}");
+                }
+                return result;
+            }
+        }
+
+    }
+}
